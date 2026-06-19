34b287c8c102422b9374f9547871478c23e442d2
Peter Hull <peterhull90@gmail.com>
2026-06-19 11:40:18 +0100

    Experimental use QuickJs4J instead of Graal.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module CanDownFX {
+    exports candown;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.media;
+    requires java.logging;
+    requires java.prefs;
+    requires java.scripting;
+    requires quickjs4j;
+    requires quickjs4j.scripting.experimental;
+}
